563a06043cf49e1cd3533d8074f31e442f6c063a
nihadaslanli <nihadsasl@code.edu.az>
2024-02-28 17:02:43 +0400

    Add project files.

---
+++ #hometask_array_datetime_method/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region task1
+//int[] array = { 1, 2, 3, 4, 5 };
+//int[] newarray = new int[array.Length];
+//int count = 0;
+
+//for (int i = array.Length - 1; i >= 0; i--)
+//{
+//    newarray[count] = array[i];
+//    count++;
+//}
+
+//foreach (int num in newarray)
+//{
+//    Console.WriteLine(num);
+//}
+
+#endregion
+#region task2
+
+//Console.WriteLine(" tarix:yyyy-MM-dd");
+//string dobInput = Console.ReadLine();
+
+//DateTime dob;
+//if (DateTime.TryParse(dobInput, out dob))
+//{
+//    DateTime now = DateTime.Now;
+
+//    int age = now.Year - dob.Year;
+
+//    if (dob > now.AddYears(-age))
+//    {
+//        age--;
+//    }
+// Console.WriteLine( age);
+//}
+//else
+//{
+//    Console.WriteLine("tarixi duzgun yaz.");
+//}
+#endregion
